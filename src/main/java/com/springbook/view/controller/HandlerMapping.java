892b2fa1391db8f2a31030555f4ee3700377a605
ukl0114 <ukl0114@naver.com>
2024-08-26 10:18:05 +0900

    13일째

--- src/main/java/com/springbook/view/controller/HandlerMapping.java
+++ src/main/java/com/springbook/view/controller/HandlerMapping.java
@@ -16,13 +16,13 @@ public class HandlerMapping {
 
 	public HandlerMapping() {
 		mappings = new HashMap<String, Controller>();
-		mappings.put("/login.do", new LoginController());
-		mappings.put("/getBoardList.do", new GetBoardListController());
-		mappings.put("/getBoard.do", new GetBoardController());
-		mappings.put("/insertBoard.do", new InsertBoardController());
-		mappings.put("/updateBoard.do", new UpdateBoardController());		
-		mappings.put("/deleteBoard.do", new DeleteBoardController());
-		mappings.put("/logout.do", new LogoutController());		
+		mappings.put("/login.do", (Controller) new LoginController());
+		mappings.put("/getBoardList.do", (Controller) new GetBoardListController());
+		mappings.put("/getBoard.do", (Controller) new GetBoardController());
+		mappings.put("/insertBoard.do", (Controller) new InsertBoardController());
+		mappings.put("/updateBoard.do", (Controller) new UpdateBoardController());		
+		mappings.put("/deleteBoard.do", (Controller) new DeleteBoardController());
+		mappings.put("/logout.do", (Controller) new LogoutController());		
 	}
 
 	public Controller getController(String path) {
