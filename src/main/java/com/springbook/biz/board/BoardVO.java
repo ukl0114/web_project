9d34a074a6e85d67cd4aa3009d62b668fda80091
ukl0114 <ukl0114@naver.com>
2024-08-31 10:48:45 +0900

    18일째

--- src/main/java/com/springbook/biz/board/BoardVO.java
+++ src/main/java/com/springbook/biz/board/BoardVO.java
@@ -2,6 +2,8 @@ package com.springbook.biz.board;
 
 import java.sql.Date;
 
+import org.springframework.web.multipart.MultipartFile;
+
 //VO(Value Object)
 public class BoardVO {
 	private int seq;
@@ -12,6 +14,7 @@ public class BoardVO {
 	private int cnt;
 	private String searchCondition;
 	private String searchKeyword;
+	private MultipartFile uploadFile;
 
 	public int getSeq() {
 		return seq;
@@ -75,6 +78,14 @@ public class BoardVO {
 
 	public void setSearchKeyword(String searchKeyword) {
 		this.searchKeyword = searchKeyword;
+	}	
+
+	public MultipartFile getUploadFile() {
+		return uploadFile;
+	}
+
+	public void setUploadFile(MultipartFile uploadFile) {
+		this.uploadFile = uploadFile;
 	}
 
 	@Override
